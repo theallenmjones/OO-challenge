fef442e0338d61e9d1fcf1afb5fafaacf06495d1
Allen Michael Jones <theallenmjones@gmail.com>
2023-04-02 01:31:09 -0500

    Add files via upload

---
+++ oo.js
@@ -0,0 +1,61 @@
+class Vehicle {
+    constructor(make, model, year) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  
+    honk() {
+      return "Beep.";
+    }
+  
+    toString() {
+      return `This vehicle is a ${this.make} ${this.model} from (${this.year})`;
+    }
+  }
+
+  //for example const myCar = new Vehicle("Toyota", "Camry", 2022);
+//  console.log(myCar.toString()); // "Toyota Camry (2022)"
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 4;
+    }
+  }
+  
+  class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 2;
+    }
+  
+    revEngine() {
+      return "VROOM!!!";
+    }
+  }
+  
+
+
+
+
+class Garage {
+    constructor(capacity) {
+      this.vehicles = [];
+      this.capacity = capacity;
+    }
+  
+    add(vehicle) {
+      if (!(vehicle instanceof Vehicle)) {
+        return "Only vehicles are allowed in here!";
+      }
+  
+      if (this.vehicles.length >= this.capacity) {
+        return "Sorry, we're full.";
+      }
+  
+      this.vehicles.push(vehicle);
+      return "Vehicle added!";
+    }
+  }
+  
